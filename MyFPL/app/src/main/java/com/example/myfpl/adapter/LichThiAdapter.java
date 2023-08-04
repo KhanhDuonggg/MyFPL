88dbfcbf0e4c5065d5d6c1c1a9aaf8e944fab82e
ThanhTung060303 <ngthanhtung060303@gmail.com>
2023-08-04 19:42:59 +0700

    giaodienlichthi

--- MyFPL/app/src/main/java/com/example/myfpl/adapter/LichThiAdapter.java
+++ MyFPL/app/src/main/java/com/example/myfpl/adapter/LichThiAdapter.java
@@ -3,9 +3,11 @@ package com.example.myfpl.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myfpl.R;
@@ -16,6 +18,7 @@ import java.util.List;
 public class LichThiAdapter extends RecyclerView.Adapter<LichThiAdapter.LichThiViewHolder> {
 
     private List<LichThi> lichThiList;
+    private boolean isShow = false;
 
     public LichThiAdapter(List<LichThi> lichThiList) {
         this.lichThiList = lichThiList;
@@ -30,21 +33,22 @@ public class LichThiAdapter extends RecyclerView.Adapter<LichThiAdapter.LichThiV
     @NonNull
     @Override
     public LichThiViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_lich_thi, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_lich_thitung, parent, false);
         return new LichThiViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull LichThiViewHolder holder, int position) {
         LichThi lichThi = lichThiList.get(position);
-        holder.tvLopThi.setText(lichThi.getLop());
+        holder.tvLopThi.setText("Lớp: "+lichThi.getLop());
         holder.tvPhongThi.setText(lichThi.getPhong());
-        holder.tvCaThi.setText(lichThi.getCa_thi());
+        holder.tvCaThi.setText("Ca: "+lichThi.getCa_thi());
         holder.tvNgayThi.setText(lichThi.getNgay());
         holder.tvMaMonThi.setText(lichThi.getMa_mon());
         holder.tvTenMonThi.setText(lichThi.getTen_mon());
         holder.tvDotThi.setText(lichThi.getDot_thi());
         holder.tvGV1.setText(lichThi.getGv1());
+        holder.cardView.setVisibility(View.GONE);
     }
 
     @Override
@@ -54,6 +58,8 @@ public class LichThiAdapter extends RecyclerView.Adapter<LichThiAdapter.LichThiV
 
     public class LichThiViewHolder extends RecyclerView.ViewHolder {
         private TextView tvLopThi, tvPhongThi, tvCaThi, tvNgayThi, tvMaMonThi, tvTenMonThi, tvDotThi, tvGV1;
+        CardView cardView;
+        ImageView imgShow;
 
         public LichThiViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -65,6 +71,24 @@ public class LichThiAdapter extends RecyclerView.Adapter<LichThiAdapter.LichThiV
             tvTenMonThi = itemView.findViewById(R.id.tvTenMonThi);
             tvDotThi = itemView.findViewById(R.id.tvDotThi);
             tvGV1 = itemView.findViewById(R.id.tvGV1);
+            cardView = itemView.findViewById(R.id.cardview_lich_thi);
+            imgShow = itemView.findViewById(R.id.imgShowDetail_lichthi);
+
+            imgShow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(!isShow){
+                        imgShow.setImageResource(R.drawable.down);
+                        cardView.setVisibility(View.VISIBLE);
+                        isShow=true;
+                    }
+                    else{
+                        imgShow.setImageResource(R.drawable.right);
+                        cardView.setVisibility(View.GONE);
+                        isShow=false;
+                    }
+                }
+            });
         }
     }
 }
